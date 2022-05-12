84ddb9873b8450d676f5d70c8870ad09e700e3a6
yash4121 <103490761+yash4121@users.noreply.github.com>
2022-05-12 17:45:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor(randomNumber1*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var images1 = "images/dice" + randomNumber1 + ".png";
+var images2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".dice .img1").setAttribute("src", images1);
+document.querySelector(".dice .img2").setAttribute("src", images2);
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 Wins!🚩";
+}
+else if(randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
